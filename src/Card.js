0362f7fa513ddddeacf0f8c77b9f0f6e2ad636b6
Christian Zintel <c.zintel@yahoo.de>
2021-06-28 23:26:05 +0200

    update cards

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,28 @@
+import { useState } from 'react'
 import './Card.css'
 
 export default function Card({ name, images, types }) {
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <section className="Card">
       <h2 className="Card__Name">{name}</h2>
-      <img className="Card__Image" src={images.front} alt="" />
-      <img className="Card__Image" src={images.back} alt="" />
-      <ul className="Card__Types-List">
-        {types.map(type => (
-          <li key={type} className="Card__Types-List--Item">
-            {type}
-          </li>
-        ))}
-      </ul>
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? 'Hide' : 'Show'}
+      </button>
+      {isActive && (
+        <>
+          <img className="Card__Image" src={images.front} alt="" />
+          <img className="Card__Image" src={images.back} alt="" />
+          <ul className="Card__Types-List">
+            {types.map(type => (
+              <li key={type} className="Card__Types-List--Item">
+                {type}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </section>
   )
 }
